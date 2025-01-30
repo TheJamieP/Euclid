45711ad4ce4b0a5d6b17ea26476cd1c6a5d8b9e9
TheJamieP <Jamieelliotpunshon@gmail.com>
2025-01-30 10:34:40 +0000

    Recursive conversion

--- Main.c
+++ Main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-int main() {
-	int a = 1100;
-	int b = 30000;
-	int r = 1;
 
-	int init_a = a;
-	int init_b = b;
-	while(r > 0){
-		r = a - b * div(a,b);
-		a = b;
-		b = r;
-	};
-	printf("gcd(%d,%d) = %d\n", init_a, init_b, a);
+int recursiveEuclid(int a, int b){
+	if (b < 1) return a;
+
+	return recursiveEuclid(b, a - b * (a/b));
+}
+int main() {
+	int a = 2024;
+	int b = 612;
+	printf("gcd(%d,%d) = %d\n", a, b, recursiveEuclid(a,b));
 }
